f48a840e156bacffa378442a44957130e3020660
Bennie Copeland <bennie@DEV-MACOSX>
2013-09-18 00:34:01 +0200

    Initial project creation.

---
+++ Proof Of Concept/Proof Of Concept/BRCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRCAppDelegate.h
+//  Proof Of Concept
+//
+//  Created by Bennie Copeland on 9/18/13.
+//  Copyright (c) 2013 Bennie Copeland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Proof Of Concept/Proof Of Concept/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Proof Of Concept
+//
+//  Created by Bennie Copeland on 9/18/13.
+//  Copyright (c) 2013 Bennie Copeland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRCAppDelegate class]));
+    }
+}
